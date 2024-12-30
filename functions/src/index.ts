1928c1dd348f0cc58a49a5cc4ef2ae1ba6206c52
YOHEI OKAYA <yohei.okaya@yoheiokaya.com>
2024-12-30 16:12:57 +0900

    add secret

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,19 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions/v2";
 import * as logger from "firebase-functions/logger";
+import {defineSecret} from "firebase-functions/params";
+
+const testSecret = defineSecret("TEST_SECRET")
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions
+.https
+.onRequest({ region: 'asia-northeast1', secrets: [testSecret] },
+    (request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!: " + testSecret.value());
+});
